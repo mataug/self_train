38aaad23f58efd8c2214033c43065d8bf8150288
Gautam Singh <gautam2004@gmail.com>
2020-05-02 00:47:56 -0500

    Add files from zip & about+index js files

---
+++ ps-flux/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Random String</h1>
+      <p>Ultra responsive stuff</p>
+    </div>
+  );
+}
+
+export default HomePage;
